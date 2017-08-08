d9091cc6c7f9c5db5d2ded359f164439d82d670b
Chelsea Holland Komlo <me@chelseakomlo.com>
2017-08-08 20:29:02 +0000

    round down odd prefixes

--- nomad/resources_endpoint.go
+++ nomad/resources_endpoint.go
@@ -73,6 +73,15 @@ func getResourceIter(context, prefix string, ws memdb.WatchSet, state *state.Sta
 	}
 }
 
+// If the length of a string is odd, return a subset of the string to the last
+// even character (n-1)
+func roundDownIfOdd(s string) string {
+	if len(s)%2 == 0 {
+		return s
+	}
+	return s[:len(s)-1]
+}
+
 // List is used to list the resouces registered in the system that matches the
 // given prefix. Resources are jobs, evaluations, allocations, and/or nodes.
 func (r *Resources) List(args *structs.ResourceListRequest,
@@ -94,7 +103,7 @@ func (r *Resources) List(args *structs.ResourceListRequest,
 			}
 
 			for _, e := range contexts {
-				iter, err := getResourceIter(e, args.Prefix, ws, state)
+				iter, err := getResourceIter(e, roundDownIfOdd(args.Prefix), ws, state)
 				if err != nil {
 					return err
 				}
